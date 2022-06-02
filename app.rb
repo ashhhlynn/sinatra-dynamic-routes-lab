859103819e52b6dadf177ba52786edf52f977d64
ashhhlynn <ashleylynnwelch@gmail.com>
2022-06-01 20:15:44 -0500

    commit tree 4273167d7a671093e529d12945d6e51b016f4535

--- app.rb
+++ app.rb
@@ -1,6 +1,27 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
-  # Write your code here!
+
+  get '/reversename/:name' do
+    @name= params[:name].reverse
+    "#{@name}"
+  end
+
+  get '/square/:number'   do 
+  @square = params[:number].to_i * params[:number].to_i
+    "#{@square}"
+  end
+
+
+
+    get '/say/:number/:phrase' do
+      @string = ''
+      params[:number].to_i.times do
+        @string += params[:phrase]
+      end
+     "{#@string}"
+    end
+
+
 
 end
